f64bb13438b5d0e266ae4ede3f5b88dacaa0eb25
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-04-29 00:47:58 +0530

    comments

--- src/Components/ContactPages/Contact.tsx
+++ src/Components/ContactPages/Contact.tsx
@@ -1,8 +1,9 @@
 const Contact = (props: any) => {
     return (
-        <div>
+        // displaying contacts from state
+        <div>            
             <button className="btn btn-secondary form-control">
-                {props.contacts.name}   // displaying contacts from state
+                {props.contacts.name}   
             </button>
         </div>
     );
