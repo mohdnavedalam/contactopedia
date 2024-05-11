fca28fc652b7562c78dbbd6432382d7050124f8c
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-05-11 21:37:21 +0530

    Axios Call

--- src/Components/ContactPages/AddRandomContact.tsx
+++ src/Components/ContactPages/AddRandomContact.tsx
@@ -1,7 +1,14 @@
+import { getRandomUser } from "../../Utilities/api";
+
+const GetRandomContact = async () => {
+    const responseFromApi = await getRandomUser();
+    console.log(responseFromApi);
+};
+
 const AddRandomContact = (props: any) => {
     return (
         <div>
-            <button className="btn btn-success form-control">
+            <button className="btn btn-success form-control" onClick={() => GetRandomContact()} >
                 Add Random Contact
             </button>
         </div>
