803e5425311d8a59db4f34110b507f1eb776bc3c
Mohd Naved Alam <mohdnavedalam1987@gmail.com>
2024-05-11 21:45:59 +0530

    Add Random Contact - Final

--- src/Components/ContactPages/AddRandomContact.tsx
+++ src/Components/ContactPages/AddRandomContact.tsx
@@ -1,14 +1,19 @@
 import { getRandomUser } from "../../Utilities/api";
 
-const GetRandomContact = async () => {
+const GetRandomContact = async (props: any) => {
     const responseFromApi = await getRandomUser();
     console.log(responseFromApi);
+    return props.handleAddRandomContact({
+        name: responseFromApi.data.first_name + " " + responseFromApi.data.last_name,
+        email: responseFromApi.data.email,
+        phone: responseFromApi.data.phone_number,
+    })
 };
 
 const AddRandomContact = (props: any) => {
     return (
         <div>
-            <button className="btn btn-success form-control" onClick={() => GetRandomContact()} >
+            <button className="btn btn-success form-control" onClick={() => GetRandomContact(props)} >
                 Add Random Contact
             </button>
         </div>
